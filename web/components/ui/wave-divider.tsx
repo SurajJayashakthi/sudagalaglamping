1b55a73ae2eb93a41384a901e85f1a8f14948a87
Suraj Jayashakthi <suraj.jayashakthi@example.com>
2026-02-15 10:26:42 +0530

    Fix hydration error in WaveDivider component

--- web/components/ui/wave-divider.tsx
+++ web/components/ui/wave-divider.tsx
@@ -7,17 +7,22 @@ interface WaveDividerProps {
 
 export function WaveDivider({ className = '', fill = 'currentColor' }: WaveDividerProps) {
     return (
-        <div className={`absolute bottom-0 left-0 right-0 w-full overflow-hidden leading-[0] ${className}`}>
+        <div
+            className={`absolute bottom-0 left-0 right-0 w-full overflow-hidden leading-[0] ${className}`}
+            suppressHydrationWarning
+        >
             <svg
                 className="relative block w-full h-[60px] md:h-[100px]"
                 viewBox="0 0 1200 120"
                 preserveAspectRatio="none"
                 xmlns="http://www.w3.org/2000/svg"
+                suppressHydrationWarning
             >
                 <path
                     d="M0,0 C150,80 350,80 600,40 C850,0 1050,0 1200,40 L1200,120 L0,120 Z"
                     fill={fill}
                     className="transition-all"
+                    suppressHydrationWarning
                 />
             </svg>
         </div>
